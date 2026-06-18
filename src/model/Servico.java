3078618e8c201595868e874af323864d6fd9d4f1
Leonardo Fenrnades <codingleonardo003fernandes@gmail.com>
2026-06-17 22:36:42 -0300

    Update Servico.java

--- src/model/Servico.java
+++ src/model/Servico.java
@@ -10,13 +10,12 @@ public class Servico implements BanhoTosa, Serializable {
     private double preco;
     private int duracaoMinutos;
 
-    // Construtor do Grupo (Não mude para não quebrar o código deles)
+
     public Servico(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    // Seu Construtor (Para o seu CRUD)
     public Servico(int id, String nome, double preco, int duracaoMinutos) {
         this.id = id;
         this.nome = nome;
